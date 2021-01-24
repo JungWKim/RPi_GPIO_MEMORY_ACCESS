1f65afeb308b7f8119ce0810a3c6d3bc934721d2
JungWKim <lionmini@naver.com>
2021-01-24 20:06:30 +0900

    .

--- application/textLcd/textLcd.c
+++ application/textLcd/textLcd.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include <sys/mman.h>
 #include <fcntl.h>
 
 #define GPIO_BASE 0xfe200000
-#define GPFSEL
-#define GPSET
+#define GPFSEL0   0x00
+#define GPFSEL1   0x04
+#define GPFSEL2   0x08
+#define GPSET0    0x1c
+#define GPCLR0    0x28
 
 volatile unsigned int *gpioBase;
-volatile unsigned int *gpioBase;
-volatile unsigned int *gpioBase;
-volatile unsigned int *gpioBase;
+volatile unsigned int *gpfsel0;
+volatile unsigned int *gpfsel1;
+volatile unsigned int *gpfsel2;
+volatile unsigned int *gpset0;
+volatile unsigned int *gpclr0;
 
 int main(void)
 {
@@ -27,7 +33,12 @@ int main(void)
         return -1;
     }
 
-
+    gpioBase = (volatile unsigned int*)gpio_memory_map;
+    gpfsel0 = gpioBase + (GPFSEL0/4);
+    gpfsel1 = gpioBase + (GPFSEL1/4);
+    gpfsel2 = gpioBase + (GPFSEL2/4);
+    gpset0 = gpioBase + (GPSET0/4);
+    gpclr0 = gpioBase + (GPCLR0/4);
 
     close(fd);
     return 0;
